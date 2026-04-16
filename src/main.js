c03c4a3b5afe4232fe5113b851f026e8b48fb296
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-04-16 18:53:36 +0300

    ...

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import k from "./kaplayContext.js";
 import { dialogData, SCENES } from "./utils/constants.js";
 import { fetchData } from "./utils/utils.js";
 
-// TODO: Rename the project title and update `index.html` <title>.
-// TODO: Decide on a fixed resolution vs responsive scaling strategy.
+// TODO: Decide on a responsive scaling strategy.
 // TODO: Configure gravity once platforming or physics are added.
 
 // TODO: Add a real asset loading pipeline (sprites, fonts, audio).
@@ -47,7 +46,7 @@ async function loadAssets(k) {
     ]);
 }
 
-async function run(k) {
+export async function run(k) {
     await loadAssets(k);
     for (const scene in SCENES) {
         k.scene(scene, () => SCENES[scene](k));
