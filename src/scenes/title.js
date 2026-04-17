ef435cb440a76c688be469a9fc09e3c61104109d
Mamoon Akhtar <mamoon.akhtar97@gmail.com>
2026-04-17 01:44:28 -0400

    Adding blue background

--- src/scenes/title.js
+++ src/scenes/title.js
@@ -1,6 +1,9 @@
 import { createButton } from "../ui/button";
+import { colorizeBG } from "../utils/utils.js";
 
 export default function titleScene(k) {
+    colorizeBG(k, 121, 192, 234);
+
     k.add([
         k.text("Naturonics", { size: 48, font: "sinko" }),
         k.anchor("center"),
