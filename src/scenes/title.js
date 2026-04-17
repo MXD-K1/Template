7e7756e45ded4e1ba64a110023df0659e03ce800
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-04-17 08:40:55 +0300

    moved createButton t o a separate ui file

--- src/scenes/title.js
+++ src/scenes/title.js
@@ -1,14 +1,12 @@
-import { createButton } from "../utils/utils";
+import { createButton } from "../ui/button.js";
 
 export default function titleScene(k) {
     k.add([
         k.text("Naturonics", { size: 48, font: "sinko" }),
         k.anchor("center"),
-        k.pos(k.width() / 2, 40)
+        k.pos(k.width() / 2, 40),
     ]);
 
     createButton(k, "Start Game", k.height() / 2, () => k.go("world"));
     createButton(k, "Options", k.height() / 2 + 80, () => k.go("load"));
-
-    
 }
